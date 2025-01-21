27d26e56832b8c9a576374124893abc7578fd3c9
xenogen <151805539+superbryn@users.noreply.github.com>
2025-01-22 02:21:31 +0530

    added something

--- src/elements/Textcard.jsx
+++ src/elements/Textcard.jsx
@@ -3,7 +3,7 @@ import './Textcard.css'
 function Textcard() {
   return (
     <div className="textcard">
-        <p className='question'>Whats SELDA?</p>
+        <p className='question'>What's SELDA?</p>
         <p><span className='firstIntro'>Sign Expression Language Digital Assistant (SELDA)<br /> 
         is an innovative AI-driven project designed to bridge <br />the communication gap between hearing individuals<br /> 
          and the deaf community<span className='teamdot'>.</span></span>
