7e9c18352f0b7d3905d3d3dac107747285c08c98
xenogen <151805539+superbryn@users.noreply.github.com>
2025-01-23 02:41:33 +0530

    take 3

--- src/selda.jsx
+++ src/selda.jsx
@@ -7,9 +7,6 @@ const Selda = () => {
   const [prediction, setPrediction] = useState('');
   const [image, setImage] = useState(null);
 
-  const goToVimeo = () => {
-    window.locate.assign("www.youtube.com")
-  }
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -42,6 +39,9 @@ const Selda = () => {
   const navigateToHome = () => {
         navigate('/');
     }
+  const goToYoutube = () => {
+    navigate("https://youtube.com")
+  }
 
   return (
     <div className='seldabody'>
@@ -51,7 +51,7 @@ const Selda = () => {
       </div>
       <nav className="navbar">
         <button onClick={navigateToHome} >Home</button>
-        <button onClick={goToVimeo} >Video</button>
+        <button onClick={goToYoutube} >Video</button>
         <button >Contibute</button>
       </nav>
     </header>
